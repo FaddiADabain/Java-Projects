71d741efc431f4b78a6362b0782a10fb0525e789
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2022-01-16 07:18:46 -0500

    Update RandomWord.java

--- Hang Man/RandomWord.java
+++ Hang Man/RandomWord.java
@@ -3,31 +3,27 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class RandomWord {
-	// Picks a random word from the text file and turns it into a char array for easy comparisons
-	public char[] randomWordPicker()
-	{
-		String dir = System.getProperty("user.dir");
-		File wordFile = new File(dir + "\\src\\words.txt");
-		RandomAccessFile file;
-		char[] word = null;
+    // Picks a random word from the text file and turns it into a char array for easy comparisons
+    public char[] randomWordPicker() {
+        String dir = System.getProperty("user.dir");
+        File wordFile = new File(dir + "\\src\\words.txt");
+        char[] word = null;
 
-		try {
-			file = new RandomAccessFile(wordFile, "r");
-			long pos = (long) (Math.random() * file.length());
-			file.seek(pos);
-			file.readLine();
-			String randomWord = file.readLine();
-			file.close();
+        try(RandomAccessFile file = new RandomAccessFile(wordFile, "r")) 
+        {
+            long pos = (long) (Math.random() * file.length());
+            file.seek(pos);
+            file.readLine();
+            String randomWord = file.readLine();
 
-			word = new char[randomWord.length()];
+            word = new char[randomWord.length()];
 
-			for(int i = 0; i < randomWord.length(); i++)
-				word[i] = randomWord.charAt(i);
+            for (int i = 0; i < randomWord.length(); i++)
+                word[i] = randomWord.charAt(i);
 
-		} catch (IOException e) 
-		{
-			e.getMessage();
-		}
-		return word;
-	}
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return word;
+    }
 }
